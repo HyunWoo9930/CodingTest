3c68955a975f44073a3101c14ef426f3bb57ec62
dhgusdn <hw62459930@gmail.com>
2021-12-07 22:16:49 +0900

    [UPDATE] 2021/12/07 - mock_exam - phonekemon - failure

---
+++ src/CodingTest/Level1/CodingTest_failure.java
@@ -0,0 +1,20 @@
+package CodingTest.Level1;
+//TODO
+public class CodingTest_failure {
+    public static void main(String[] ar) {
+        int N = 5;
+        int[] stages = {2,1,2,5,2,4,3,3};
+        int count= 0;
+        for(int i = 0; i < stages.length;i++) {
+            for(int j = 0; j < stages.length;j++) {
+                if(stages[i] <= stages[j]) {
+                    count++;
+                    System.out.println("stages[i] : " + stages[i] + " stages[j] : " + stages[j]);
+                }
+
+            }System.out.println(count);
+            count = 0;
+        }
+
+    }
+}
