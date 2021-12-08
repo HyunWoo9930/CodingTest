d6f8d92e550f67de6d670a3bcba8f436ef6734b7
hyunwoo <hw62459930@gmail.com>
2021-12-08 17:16:44 +0900

    [CodingTest] 2021/12/08 - Athletes not finish

--- src/CodingTest/Level1/CodingTest_Lotto.java
+++ src/CodingTest/Level1/CodingTest_Lotto.java
@@ -3,13 +3,16 @@ package CodingTest.Level1;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CodingTest_Lotto {
 
   public static void main(String[] ar) {
-    List<Integer> lottos = new ArrayList<Integer>(Arrays.asList(44, 1, 0, 0, 31, 25));
-    List<Integer> win_nums = new ArrayList<Integer>(Arrays.asList(31, 10, 45, 1, 6, 19));
-    List<Integer> result = new ArrayList<>();
+    int[] lotto = {45, 4, 35, 20, 3, 9};
+    int[] win_num = {20, 9, 3, 45, 4, 35};
+    List<Integer> lottos = Arrays.stream(lotto).boxed().collect(Collectors.toList());
+    List<Integer> win_nums = Arrays.stream(win_num).boxed().collect(Collectors.toList());
+    int[] result = new int[2];
 
     int count = 0;
     int imagine = 0;
@@ -19,32 +22,35 @@ public class CodingTest_Lotto {
         if(lottos.get(i) == win_nums.get(j)) {
           lottos.remove(i);
           count++;
+
         }
       }
     }
-
+    System.out.println(lottos);
+    System.out.println(count);
     for(int i = 0; i< lottos.size();i++) {
       if(lottos.get(i) == 0) {
         imagine++;
       }
     }
+    System.out.println(imagine);
     int MaxCount = imagine + count;
     int MinCount = count;
 
-    s(MaxCount,result);
-    s(MinCount,result);
+    s(MaxCount,0,result);
+    s(MinCount,1,result);
 
-    System.out.println(result);
+    System.out.println(result[0] +" : "+ result[1]);
 
 
   }
-  public static void s(int result_count, List<Integer> results) {
-    if(result_count == 6) results.add(1);
-    else if(result_count == 5) results.add(2);
-    else if(result_count == 4) results.add(3);
-    else if(result_count == 3) results.add(4);
-    else if(result_count == 2) results.add(5);
-    else results.add(6);
+  public static void s(int result_count,int n, int[] results) {
+    if(result_count == 6) results[n] = 1;
+    else if(result_count == 5)  results[n] = 2;
+    else if(result_count == 4)  results[n] = 3;
+    else if(result_count == 3)  results[n] = 4;
+    else if(result_count == 2)  results[n] = 5;
+    else  results[n] = 6;
   }
 
 
--- src/CodingTest/Level1/CodingTest_phonekemon.java
+++ src/CodingTest/Level1/CodingTest_phonekemon.java
@@ -7,22 +7,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CodingTest_phonekemon {
-    public static void main(String[] ar) {
-        int[] nums = {3,1,2,3};
-        int getPhonekemons = nums.length / 2;
-        int answer = 0;
-        List<Integer> monList = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        monList = monList.stream().distinct().collect(Collectors.toList());
-        System.out.println(monList);
-        if(monList.size() >= getPhonekemons) {
-            answer = getPhonekemons;
-        }
-        else {
-            answer = monList.size();
-        }
 
-
-
-
+  public static void main(String[] ar) {
+    int[] nums = {3, 1, 2, 3};
+    int getPhonekemons = nums.length / 2;
+    int answer = 0;
+    List<Integer> monList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+    monList = monList.stream().distinct().collect(Collectors.toList());
+    System.out.println(monList);
+    if (monList.size() >= getPhonekemons) {
+      answer = getPhonekemons;
+    } else {
+      answer = monList.size();
     }
+
+
+  }
 }
