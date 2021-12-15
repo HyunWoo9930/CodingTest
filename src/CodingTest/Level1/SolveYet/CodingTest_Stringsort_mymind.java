cb523e8bda92f8373994190bfffd63f59614a526
hyunwoo <hw62459930@gmail.com>
2021-12-15 18:03:33 +0900

    [CodingTest] 2021/12/15 - ....

--- src/CodingTest/Level1/SolveYet/CodingTest_Stringsort_mymind.java
+++ src/CodingTest/Level1/SolveYet/CodingTest_Stringsort_mymind.java
@@ -2,25 +2,25 @@ package CodingTest.Level1.SolveYet;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CodingTest_Stringsort_mymind {
   public static void main(String[] ar) {
-    String[] strings = {"ae","ae","be","ce"};
+    String[] strings = {"sun", "bed", "car"};
     int n = 1;
     String[] strings2_2 = new String[strings.length];
-    String[] answer = new String[strings.length];
-    String[] strings2_1 = new String[strings.length];
-
+    Map<String,String> map = new HashMap<>();
     for(int i = 0;i<strings.length;i++) {
-      strings2_1[i] = strings[i].substring(0,n);
-      strings2_2[i] = strings[i].substring(n);
+      strings2_2[i] = strings[i].substring(n)+strings[i].substring(0,n);
+      System.out.println(strings2_2[i]);
+      map.put(strings2_2[i],strings[i]);
     }
-    Arrays.sort(strings2_2);
-    for(int i = 0;i<strings.length;i++) {
-      strings2_2[i] += strings2_1[i];
-      answer[i] = strings2_2[i].substring(strings[i].length()-n) + strings2_2[i].substring(0,strings[i].length()-n);
-      System.out.println(answer[i]);
+    Object[] mapKey = map.keySet().toArray();
+    Arrays.sort(mapKey);
+    for(String keyVal : map.keySet()) {
+      System.out.println(map.get(keyVal));
     }
   }
 }
