b04b326ff5519a98d80ec949edc2b49f78cbcca1
dhgusdn <hw62459930@gmail.com>
2022-04-07 11:27:56 +0900

    2022.04.07

---
+++ src/CodingTest/BeakJun/SolvedYet/Class2/SortInside.java
@@ -0,0 +1,22 @@
+package CodingTest.BeakJun.SolvedYet.Class2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class SortInside {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String n = sc.next();
+    List<Integer> list = new ArrayList<>();
+    for (int i = 0; i < n.length(); i++) {
+      list.add(Integer.parseInt(n.split("")[i]));
+    }
+    Collections.sort(list);
+    for(int i = list.size()-1 ; i >= 0;i--) {
+      System.out.print(list.get(i));
+    }
+  }
+}
